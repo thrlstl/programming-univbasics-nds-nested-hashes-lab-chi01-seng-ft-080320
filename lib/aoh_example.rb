688b1f86f8fee8508ade19c80ee48c39536f945a
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-27 06:37:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -37,6 +37,6 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  assembled_aoh[row][key] = "new value"
-  assembled_aoh
+  aoh[row][key] = "new value"
+  aoh
 end
